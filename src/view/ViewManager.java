4c69c7abc697247927b03b9ab43037ed541c0c3c
Andres Maldonado <andres.maldonado02@uptc.edu.co>
2023-08-17 21:08:56 -0500

    Vista v1.0

--- src/view/ViewManager.java
+++ src/view/ViewManager.java
@@ -1,7 +1,25 @@
 package view;
 
-import javax.swing.*;
+import javax.swing.JOptionPane;
 
-public class ViewManager{
-	
+public class ViewManager {
+    public void showGraphicMessage(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
+
+    public double readGraphicDouble(String message) {
+        return Double.parseDouble(JOptionPane.showInputDialog(message));
+    }
+
+    public long readGraphicLong(String message) {
+        return Long.parseLong(JOptionPane.showInputDialog(message));
+    }
+
+    public short readGraphicShort(String message) {
+        return Short.parseShort(JOptionPane.showInputDialog(message));
+    }
+
+    public void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(null, message, "Error", JOptionPane.ERROR_MESSAGE);
+    }
 }
